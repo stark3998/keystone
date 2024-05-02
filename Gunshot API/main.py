1c033d440045a3fb9ca5aa64c103801b0b2a4491
Jatin Madan <jatin.madan39@gmail.com>
2024-05-02 10:43:47 -0700

    Added Upload Audio API

--- Gunshot API/main.py
+++ Gunshot API/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from pydantic import BaseModel
 
 
@@ -18,7 +18,7 @@ from include import helpers
 from keras.models import load_model # type: ignore
 
 model_file = "aug-train-nb3.keras"
-models_path = os.path.abspath("./Gunshot API/models")
+models_path = os.path.abspath("/Users/jaycrappe/Documents/GitHub/Keystone/Gunshot API/models")
 model_path = os.path.join(models_path, model_file)
 
 test_file = "/Users/jaycrappe/Documents/GitHub/urban-audio-classifier/UrbanSound8K/audio/fold2/76089-6-0-0.wav"
@@ -67,6 +67,38 @@ async def get_post_result(sound: Sound):
         sound.fold, sound.file.replace(".wav", "")
     )
     mels = helpers.get_mel_spectrogram(test_file, 0, n_mels=40)
+    mels_max_padding = 174
+    size = len(mels[1])
+    if size < mels_max_padding:
+        pad_width = mels_max_padding - size
+        mels_px = np.pad(
+            mels,
+            pad_width=((0, 0), (0, pad_width)),
+            mode="constant",
+            constant_values=(0,),
+        )
+
+    print(mels_px.shape)
+    y_probs = model.predict(mels_px.reshape(1, 40, 174, 1))
+    print(y_probs)
+    yhat_probs = np.argmax(y_probs, axis=1)
+    print(labels[yhat_probs[0]])
+    return {"Detected": labels[yhat_probs[0]]}
+
+@app.post("/upload/")
+async def get_uploaded_result(audio_file: UploadFile = File(...)):
+    if not audio_file.filename.endswith('.wav'):
+        raise HTTPException(status_code=400, detail="Invalid file format. Please upload a '.wav' file.")
+    WAVE_OUTPUT_FILENAME = "background-test.wav"
+    audio_filename = (
+            "/Users/jaycrappe/Documents/GitHub/Keystone/Gunshot API/"
+            + WAVE_OUTPUT_FILENAME
+        )
+    CHUNK = 1024
+    with open(audio_filename, "wb") as buffer:
+        while data := await audio_file.read(CHUNK):
+            buffer.write(data)
+    mels = helpers.get_mel_spectrogram(audio_filename, 0, n_mels=40)
     mels_max_padding = 174
     size = len(mels[1])
     if size < mels_max_padding:
